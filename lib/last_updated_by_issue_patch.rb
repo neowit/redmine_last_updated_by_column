9a03ceba81cced9c647ebb6859979ee50b0fc980
A. G <trash+github_NO_SPAM_@neowit.com>
2012-12-05 20:52:51 +0000

    display blank "updated by" on /issues page when Issue had no updates since creation

--- lib/last_updated_by_issue_patch.rb
+++ lib/last_updated_by_issue_patch.rb
@@ -4,8 +4,6 @@ module LastUpdatedByIssuePatch
 
 		base.send(:include, InstanceMethods)
 
-
-
 	end
 
 	module ClassMethods
@@ -14,23 +12,26 @@ module LastUpdatedByIssuePatch
 	module InstanceMethods
 
 		# add a column to the +available_columns+ that isn't provided by the core.
-		def last_updated_by
+		# @param: no_default - if true then when Issue was created and has not have any updates then
+		# instead of Creator name and Id we return nil
+		# this is used in "Updated By" column on /issues page because we want
+		# to show blank value, otherwise sort order looks weird
+		def last_updated_by (no_default = false)
 			_sql = "select CONCAT_WS(' ', u.firstname, u.lastname) as name, u.id as id from #{Journal.table_name} as j1 inner join #{User.table_name} as u on u.id = j1.`user_id` where `journalized_type` = 'Issue' and `journalized_id` = #{id} order by j1.`id` DESC limit 1"
 			result = ActiveRecord::Base.connection.select_one _sql
 			updated_by_name = ""
 			updated_by_id = nil
-			if result.nil?
-				updated_by_name = self.author.name
-				updated_by_id = self.author.id
-				updated_by_name = self.author.name
-			else
+			if !result.nil? 
 				updated_by_id = result["id"].to_i
 				updated_by_name = result["name"]
+			elsif !no_default 
+				updated_by_id = self.author.id
+				updated_by_name = self.author.name
 			end
 			@last_updated_by ||= {:name => updated_by_name, :id => updated_by_id}
 		end
 		def last_updated_by_name
-			@last_updated_by_name ||= self.last_updated_by[:name]
+			@last_updated_by_name ||= self.last_updated_by(true)[:name]
 		end
 
 	end
--- lib/last_updated_by_query_patch.rb
+++ lib/last_updated_by_query_patch.rb
@@ -4,22 +4,10 @@ module LastUpdatedByQueryPatch
 
 		base.class_eval do
 			unloadable # Send unloadable so it will not be unloaded in development
-			#_sql = "select CASE WHEN u.id = null THEN CONCAT_WS(' ', a.firstname, a.lastname) ELSE CONCAT_WS(' ', u.firstname, u.lastname) END from #{Issue.table_name} as i"
-			#_sql << " left join #{Journal.table_name} as j on j.journalized_id = i.id and journalized_type = 'Issue' "
-			#_sql << " left join #{User.table_name} as u on u.id = j.user_id"
-			#_sql <<	" left join #{User.table_name} as a on a.id = i.author_id"
-			#_sql << " where  i.id = #{Issue.table_name}.id order by j.id DESC limit 1"
-
-			_sql = "( select lastmodifiedby_name from ( "
-			_sql << " (select CONCAT_WS(' ', u.firstname, u.lastname) lastmodifiedby from #{Journal.table_name} as j1 inner join #{User.table_name} as u on u.id = j1.user_id where journalized_type = 'Issue' and journalized_id = #{Issue.table_name}.id order by j1.id DESC limit 1)"
-			_sql << " union "
-			_sql << " (select CONCAT_WS(' ', u.firstname, u.lastname) as lastmodifiedby_name from #{User.table_name} u where id = #{Issue.table_name}.author_id) "
-			_sql << " ) as lastmodifiedby limit 1 )"
 
 			base.add_available_column(QueryColumn.new(:last_updated_by_name,
 				:caption => :label_last_updated_by,
-				:sortable => _sql
-				#:sortable => "(select CONCAT_WS(' ', u.firstname, u.lastname) from #{Journal.table_name} as j1 inner join #{User.table_name} as u on u.id = j1.user_id where journalized_type = 'Issue' and journalized_id = #{Issue.table_name}.id order by j1.id DESC limit 1)"
+				:sortable => "(select CONCAT_WS(' ', u.firstname, u.lastname) from #{Journal.table_name} as j1 inner join #{User.table_name} as u on u.id = j1.user_id where journalized_type = 'Issue' and journalized_id = #{Issue.table_name}.id order by j1.id DESC limit 1)"
 			))
 		end
 
