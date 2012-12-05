0759d65e7de4f89c2cc4c57d74ffc55c28a2d9cd
A. G <trash+github_NO_SPAM_@neowit.com>
2012-12-05 18:37:10 +0000

    added last_updated_by_id method

--- lib/last_updated_by_issue_patch.rb
+++ lib/last_updated_by_issue_patch.rb
@@ -18,7 +18,15 @@ module LastUpdatedByIssuePatch
 			_sql = "select CONCAT_WS(' ', u.firstname, u.lastname) from `journals` as j1 inner join users as u on u.id = j1.`user_id` where `journalized_type` = 'Issue' and `journalized_id` = #{id} order by j1.`id` DESC limit 1"
 			result = ActiveRecord::Base.connection.select_value _sql
 			unless result.nil?
-				@last_modified_by ||= result
+				@last_updated_by ||= result
+			end
+		end
+
+		def last_updated_by_id
+			_sql = "select u.id from `journals` as j1 inner join users as u on u.id = j1.`user_id` where `journalized_type` = 'Issue' and `journalized_id` = #{id} order by j1.`id` DESC limit 1"
+			result = ActiveRecord::Base.connection.select_value _sql
+			unless result.nil?
+				@last_updated_by_id ||= result.to_i
 			end
 		end
 	end
